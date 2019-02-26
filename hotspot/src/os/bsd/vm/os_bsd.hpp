fc4a96fa9a1dc3964e98a5b77531c74a02c8b24d
Greg Lewis <glewis@eyesbeyond.com>
2019-02-25 19:16:34 -0800

    Add support for "-XX+UseLargePages" on FreeBSD
    * Remove non-functional large pages code copied from Linux.
    * Implement FreeBSD support for aligning allocated memories to superpages.
    
    Authored by:	alc@FreeBSD.org

--- hotspot/src/os/bsd/vm/os_bsd.hpp
+++ hotspot/src/os/bsd/vm/os_bsd.hpp
@@ -87,7 +87,7 @@ class Bsd {
   static void rebuild_cpu_to_node_map();
   static GrowableArray<int>* cpu_to_node()    { return _cpu_to_node; }
 
-  static bool hugetlbfs_sanity_check(bool warn, size_t page_size);
+  static bool superpage_sanity_check(bool warn, size_t *page_size);
 
  public:
 
