c632696f060867edc340a1a36297d3d7e607ece7
Greg Lewis <glewis@eyesbeyond.com>
2019-02-25 18:45:38 -0800

    Fix ARM compilation on BSD
    * Merge in a fix from os_linux_zero.cpp
    
    Authored by:	jkim@FreeBSD.org

--- hotspot/src/os_cpu/bsd_zero/vm/os_bsd_zero.cpp
+++ hotspot/src/os_cpu/bsd_zero/vm/os_bsd_zero.cpp
@@ -60,8 +60,8 @@
 #include "utilities/vmError.hpp"
 
 address os::current_stack_pointer() {
-  address dummy = (address) &dummy;
-  return dummy;
+  // return the address of the current function
+  return (address)__builtin_frame_address(0);
 }
 
 frame os::get_sender_for_C_frame(frame* fr) {
@@ -189,7 +189,7 @@ JVM_handle_bsd_signal(int sig,
       ShouldNotCallThis();
     }
     else*/ if (thread->thread_state() == _thread_in_vm &&
-               sig == SIGBUS && thread->doing_unsafe_access()) {
+               (sig == SIGSEGV || sig == SIGBUS) && thread->doing_unsafe_access()) {
       ShouldNotCallThis();
     }
 
