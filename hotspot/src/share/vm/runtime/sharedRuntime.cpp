5c4960a2512c94cc6efd6aa468792fd54b2ad1df
Kurt Miller <bsdkurt@gmail.com>
2020-11-21 12:49:42 -0500

    Fix fastdebug build with clang 10+ by suppressing some warnings.

--- hotspot/src/share/vm/runtime/sharedRuntime.cpp
+++ hotspot/src/share/vm/runtime/sharedRuntime.cpp
@@ -2630,7 +2630,7 @@ void AdapterHandlerLibrary::create_native_wrapper(methodHandle method) {
     if (buf != NULL) {
       CodeBuffer buffer(buf);
       double locs_buf[20];
-      buffer.insts()->initialize_shared_locs((relocInfo*)locs_buf, sizeof(locs_buf) / sizeof(relocInfo));
+      buffer.insts()->initialize_shared_locs((relocInfo*)locs_buf, sizeof(locs_buf) / (sizeof(relocInfo)));
       MacroAssembler _masm(&buffer);
 
       // Fill in the signature array, for the calling-convention call.
@@ -2733,7 +2733,7 @@ nmethod *AdapterHandlerLibrary::create_dtrace_nmethod(methodHandle method) {
       // Need a few relocation entries
       double locs_buf[20];
       buffer.insts()->initialize_shared_locs(
-        (relocInfo*)locs_buf, sizeof(locs_buf) / sizeof(relocInfo));
+        (relocInfo*)locs_buf, sizeof(locs_buf) / (sizeof(relocInfo)));
       MacroAssembler _masm(&buffer);
 
       // Generate the compiled-to-native wrapper code
